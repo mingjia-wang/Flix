c7fbe4542b82d959ad817747d7534b4f057b7bc7
Mingjia Wang <mingjiawangdev@gmail.com>
2020-01-21 15:13:38 -0800

    Initial Commit

---
+++ flixster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flixster
+//
+//  Created by Mingjia Wang on 1/21/20.
+//  Copyright © 2020 Mingjia Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
