ff722672e3f46bc4666b95a9b70faea162a97365
Mingjia Wang <mingjiawangdev@gmail.com>
2020-01-28 22:47:30 -0800

    Added movie details screen and superhero movies tab

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Mingjia Wang on 1/27/20.
+//  Copyright © 2020 Mingjia Wang. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
